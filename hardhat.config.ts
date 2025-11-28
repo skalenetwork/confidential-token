e45027a8b9b25b494778d8031425d3ce4b98e479
Dmytro Stebaiev <dimastebaev@gmail.com>
2025-11-28 17:13:22 +0200

    Setup typescript

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  networks: {
+    custom: {
+      url: process.env.ENDPOINT || "http://localhost:8545",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    }
+  },
+  solidity: "0.8.30",
 };
 
 export default config;
