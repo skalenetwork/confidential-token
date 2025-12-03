8309894beb900939efc51c248e40214096192a36
Dmytro Stebaiev <dimastebaev@gmail.com>
2025-12-03 17:43:56 +0200

    Fix warnings in EIP-3009

--- contracts/eip3009/EIP3009.sol
+++ contracts/eip3009/EIP3009.sol
@@ -37,50 +37,69 @@ import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 import { EIP712Utils } from "./EIP712Utils.sol";
 
+
+/// @title EIP3009
+/// @author Dmytro Stebaiev
+/// @notice ERC20 token with transfer and receive with authorization functionality
 abstract contract EIP3009 is ERC20, EIP712 {
-    // keccak256("TransferWithAuthorization(address from,address to,uint256 value,uint256 validAfter,uint256 validBefore,bytes32 nonce)")
-    bytes32
-        public constant TRANSFER_WITH_AUTHORIZATION_TYPEHASH = 0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267;
+    /// @notice typehash for transfer with authorization
+    bytes32 public constant TRANSFER_WITH_AUTHORIZATION_TYPEHASH =
+        // This is calculated during compilation time
+        // so lengthy strings are not an issue
+        // solhint-disable-next-line gas-small-strings
+        keccak256("TransferWithAuthorization("
+            "address from,"
+            "address to,"
+            "uint256 value,"
+            "uint256 validAfter,"
+            "uint256 validBefore,"
+            "bytes32 nonce)"
+        );
 
-    // keccak256("ReceiveWithAuthorization(address from,address to,uint256 value,uint256 validAfter,uint256 validBefore,bytes32 nonce)")
-    bytes32
-        public constant RECEIVE_WITH_AUTHORIZATION_TYPEHASH = 0xd099cc98ef71107a616c4f0f941f04c322d8e254fe26b3c6668db87aae413de8;
+    /// @notice typehash for receiving with authorization
+    bytes32 public constant RECEIVE_WITH_AUTHORIZATION_TYPEHASH =
+        // This is calculated during compilation time
+        // so lengthy strings are not an issue
+        // solhint-disable-next-line gas-small-strings
+        keccak256("ReceiveWithAuthorization("
+            "address from,"
+            "address to,"
+            "uint256 value,"
+            "uint256 validAfter,"
+            "uint256 validBefore,"
+            "bytes32 nonce)"
+        );
 
-    // keccak256("CancelAuthorization(address authorizer,bytes32 nonce)")
-    bytes32
-        public constant CANCEL_AUTHORIZATION_TYPEHASH = 0x158b0a9edf7a828aad02f63cd515c68ef2f50ba807396f6d12842833a1597429;
+    /// @notice typehash for canceling an authorization
+    bytes32 public constant CANCEL_AUTHORIZATION_TYPEHASH =
+        // This is calculated during compilation time
+        // so lengthy strings are not an issue
+        // solhint-disable-next-line gas-small-strings
+        keccak256("CancelAuthorization(address authorizer,bytes32 nonce)");
 
     /**
      * @dev authorizer address => nonce => state (true = used / false = unused)
      */
-    mapping(address => mapping(bytes32 => bool)) internal _authorizationStates;
+    mapping(address authorizer => mapping(bytes32 nonce => bool state)) internal _authorizationStates;
 
+    /// @notice Emitted when an authorization is used
+    /// @param authorizer    Authorizer's address
+    /// @param nonce         Nonce of the authorization
     event AuthorizationUsed(address indexed authorizer, bytes32 indexed nonce);
+
+    /// @notice Emitted when an authorization is canceled
+    /// @param authorizer    Authorizer's address
+    /// @param nonce         Nonce of the authorization
     event AuthorizationCanceled(
         address indexed authorizer,
         bytes32 indexed nonce
     );
 
-    string
-        internal constant _INVALID_SIGNATURE_ERROR = "EIP3009: invalid signature";
-    string
-        internal constant _AUTHORIZATION_USED_ERROR = "EIP3009: authorization is used";
-
-    /**
-     * @notice Returns the state of an authorization
-     * @dev Nonces are randomly generated 32-byte data unique to the authorizer's
-     * address
-     * @param authorizer    Authorizer's address
-     * @param nonce         Nonce of the authorization
-     * @return True if the nonce is used
-     */
-    function authorizationState(address authorizer, bytes32 nonce)
-        external
-        view
-        returns (bool)
-    {
-        return _authorizationStates[authorizer][nonce];
-    }
+    error InvalidSignature();
+    error AuthorizationIsNotYetValid(uint256 validAfter);
+    error AuthorizationIsExpired(uint256 validBefore);
+    error AuthorizationUsedError(address authorizer, bytes32 nonce);
+    error CallerMustBeThePayee(address caller, address payee);
 
     /**
      * @notice Execute a transfer with a signed authorization
@@ -105,18 +124,18 @@ abstract contract EIP3009 is ERC20, EIP712 {
         bytes32 r,
         bytes32 s
     ) external {
-        _transferWithAuthorization(
-            TRANSFER_WITH_AUTHORIZATION_TYPEHASH,
-            from,
-            to,
-            value,
-            validAfter,
-            validBefore,
-            nonce,
-            v,
-            r,
-            s
-        );
+        _transferWithAuthorization({
+            typeHash: TRANSFER_WITH_AUTHORIZATION_TYPEHASH,
+            from: from,
+            to: to,
+            value: value,
+            validAfter: validAfter,
+            validBefore: validBefore,
+            nonce: nonce,
+            v: v,
+            r: r,
+            s: s
+        });
     }
 
     /**
@@ -145,20 +164,20 @@ abstract contract EIP3009 is ERC20, EIP712 {
         bytes32 r,
         bytes32 s
     ) external {
-        require(to == msg.sender, "EIP3009: caller must be the payee");
+        require(to == msg.sender, CallerMustBeThePayee(msg.sender, to));
 
-        _transferWithAuthorization(
-            RECEIVE_WITH_AUTHORIZATION_TYPEHASH,
-            from,
-            to,
-            value,
-            validAfter,
-            validBefore,
-            nonce,
-            v,
-            r,
-            s
-        );
+        _transferWithAuthorization({
+            typeHash: RECEIVE_WITH_AUTHORIZATION_TYPEHASH,
+            from: from,
+            to: to,
+            value: value,
+            validAfter: validAfter,
+            validBefore: validBefore,
+            nonce: nonce,
+            v: v,
+            r: r,
+            s: s
+        });
     }
 
     /**
@@ -178,7 +197,7 @@ abstract contract EIP3009 is ERC20, EIP712 {
     ) external {
         require(
             !_authorizationStates[authorizer][nonce],
-            _AUTHORIZATION_USED_ERROR
+            AuthorizationUsedError(authorizer, nonce)
         );
 
         bytes memory data = abi.encode(
@@ -187,14 +206,47 @@ abstract contract EIP3009 is ERC20, EIP712 {
             nonce
         );
         require(
-            EIP712Utils.recover(_domainSeparatorV4(), v, r, s, data) == authorizer,
-            _INVALID_SIGNATURE_ERROR
+            EIP712Utils.recover({
+                domainSeparator: _domainSeparatorV4(),
+                v: v,
+                r: r,
+                s: s,
+                typeHashAndData: data
+            }) == authorizer,
+            InvalidSignature()
         );
 
         _authorizationStates[authorizer][nonce] = true;
         emit AuthorizationCanceled(authorizer, nonce);
     }
 
+    /**
+     * @notice Returns the state of an authorization
+     * @dev Nonces are randomly generated 32-byte data unique to the authorizer's
+     * address
+     * @param authorizer    Authorizer's address
+     * @param nonce         Nonce of the authorization
+     * @return used True if the nonce is used
+     */
+    function authorizationState(address authorizer, bytes32 nonce)
+        external
+        view
+        returns (bool used)
+    {
+        return _authorizationStates[authorizer][nonce];
+    }
+
+    /// @notice Internal function to execute transfer with authorization
+    /// @param typeHash      Type hash of the authorization
+    /// @param from          Payer's address (Authorizer)
+    /// @param to            Payee's address
+    /// @param value         Amount to be transferred
+    /// @param validAfter    The time after which this is valid (unix time)
+    /// @param validBefore   The time before which this is valid (unix time)
+    /// @param nonce         Unique nonce
+    /// @param v             v of the signature
+    /// @param r             r of the signature
+    /// @param s             s of the signature
     function _transferWithAuthorization(
         bytes32 typeHash,
         address from,
@@ -207,9 +259,13 @@ abstract contract EIP3009 is ERC20, EIP712 {
         bytes32 r,
         bytes32 s
     ) internal {
-        require(block.timestamp > validAfter, "EIP3009: authorization is not yet valid");
-        require(block.timestamp < validBefore, "EIP3009: authorization is expired");
-        require(!_authorizationStates[from][nonce], _AUTHORIZATION_USED_ERROR);
+        // Time specific logic is used
+        // to implement EIP-3009
+        // solhint-disable not-rely-on-time
+        require(block.timestamp > validAfter, AuthorizationIsNotYetValid(validAfter));
+        require(block.timestamp < validBefore, AuthorizationIsExpired(validBefore));
+        // solhint-enable not-rely-on-time
+        require(!_authorizationStates[from][nonce], AuthorizationUsedError(from, nonce));
 
         bytes memory data = abi.encode(
             typeHash,
@@ -221,8 +277,14 @@ abstract contract EIP3009 is ERC20, EIP712 {
             nonce
         );
         require(
-            EIP712Utils.recover(_domainSeparatorV4(), v, r, s, data) == from,
-            _INVALID_SIGNATURE_ERROR
+            EIP712Utils.recover({
+                domainSeparator: _domainSeparatorV4(),
+                v: v,
+                r: r,
+                s: s,
+                typeHashAndData: data
+            }) == from,
+            InvalidSignature()
         );
 
         _authorizationStates[from][nonce] = true;
